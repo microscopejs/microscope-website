9ec60cc545b9b0516e113c2df6db18147912b806
bhtz <heintz.benjamin@gmail.com>
2015-11-01 21:26:05 +0100

    refactoring previewer

--- src/components/preview/directives/preview.js
+++ src/components/preview/directives/preview.js
@@ -3,13 +3,12 @@
 function Preview($sce) {
 
     var directive = {
-
         restrict: 'EA',
         transclude: true,
-        templateUrl:'components/preview/directives/preview.html',
+        templateUrl: 'components/preview/directives/preview.html',
         scope: {
             iframeUrl: "=",
-            device:'='
+            device: '='
         },
         link: link
     };
@@ -17,29 +16,27 @@ function Preview($sce) {
     /**
      * directive view model
      */
-    function link($scope, element, attrs,ctrl,transclude) {
-        
-        transclude($scope, function(clone, scope) {
+    function link($scope, element, attrs, ctrl, transclude) {
+
+        transclude($scope, function (clone, scope) {
             element.append(clone);
         });
-      
+
         $scope.isLoading = true;
-        $scope.$watch('device',function(){
-               $scope.iframeClass = 'iframe-'+$scope.device;
-               $scope.deviceClass = 'device-'+$scope.device;
+        $scope.$watch('device', function () {
+            $scope.iframeClass = 'iframe-' + $scope.device;
+            $scope.deviceClass = 'device-' + $scope.device;
         });
-     
-        
+
         $scope.cleanUrl = $sce.trustAsResourceUrl($scope.iframeUrl);
-        
-        $scope.iframeLoaded = function(){ 
-		  $scope.isLoading = false;
-		  $scope.$apply();
+
+        $scope.iframeLoaded = function () {
+            $scope.isLoading = false;
+            $scope.$apply();
         };
     }
 
     return directive;
 }
-
 
 module.exports = Preview;
