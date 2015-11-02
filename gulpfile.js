ad944bfdedc6bc4baeef638ddd5783c5c1f285d7
bhtz <heintz.benjamin@gmail.com>
2015-11-02 10:52:59 +0100

    remove form

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,6 @@
-var gulp         = require('gulp');
-var WorkflowForm = require('./tasks/form/WorkflowForm');
+var gulp = require('gulp');
 require('./tasks/build');
 require('./tasks/serve');
 require('./tasks/test');
 require('./tasks/docs');
 require('./tasks/watch');
-
-// default task => instanciate GulpForm
-gulp.task('default', function (cb) {
-	var g = new WorkflowForm(cb);
-});
