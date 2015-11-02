9d309447ba6d1eae8fd7e037eef6c9480fea98eb
Thomas Adjadj <ta@ecofip.com>
2015-11-02 17:57:37 +0100

    maj team

--- src/components/team/team.js
+++ src/components/team/team.js
@@ -3,16 +3,14 @@
 
 // Imports
 var angular = require('angular');
-var TeamService = require('./services/teamService');
 var TeamCtrl = require('./controllers/teamCtrl');
-var Team  = require('./directives/team');
+
 
 
 // Work sub-module definition
 var team = angular.module('app.team', []);
-team.service('TeamService', ['$http', TeamService]);
 team.controller('TeamCtrl', [TeamCtrl]);
-team.directive('team',['TeamService',Team]);
+
 
 module.exports = team;
 
